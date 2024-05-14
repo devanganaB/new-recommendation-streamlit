a79877a1860b967deac189f7c8ca06cadca55703
Devangana26 <devanganabarua@gmail.com>
2024-05-14 17:47:03 +0530

    set up file created and streamlit app done

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-with open("REAME.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
@@ -12,7 +12,6 @@ setup(
     name= SRC_REPO,
     version='0.0.1',
     author=AUTHOR_NAME,
-    author_email='devanganabarua26@gmail.com',
     description='test package for movie recommendation',
     long_description=long_description,
     long_description_content_type='text/markdown',
